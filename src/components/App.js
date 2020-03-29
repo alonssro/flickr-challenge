8f9856fd3c2c655c67be9f7c6adfc68ff0d1a882
Alonso Rodriguez <david.rodriguez@konfio.mx>
2020-03-28 18:10:23 -0600

    Added endpoint for Api

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 
+import flickr from '../utils/api';
+
 export default class App extends Component {
   state = {
     images: []
   };
 
-  onSearchSubmit = async searchText => {
-    console.log(searchText);
+  onSearchSubmit = async text => {
+    console.log(text);
+    const { data: images } = await flickr.post('/photos', {
+      text
+    });
+    this.setState({
+      images
+    });
   };
 
   render() {
---
+++ src/utils/api.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: 'https://6g0pol1nm1.execute-api.us-east-1.amazonaws.com/dev'
+});
